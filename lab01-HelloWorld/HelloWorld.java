a93c8eb12a89b1a9105ce5518dbb336d6fbbd541
bal217 <bal217@lehigh.edu>
2014-08-28 22:11:45 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+//Brittany Le Jouan 
+//Demo 0
+//HelloWorld Java Program
+//
+//first compile the program
+// javac HelloWorld.java
+// java HelloWorld //
+// define a class
+public class HelloWorld{
+//add main method
+public static void main(String[] args){
+//print statement
+//print statement
+	System.out.println("########################");
+	System.out.println("########################");
+	System.out.println("#                      #");
+	System.out.println("#  Hello World 2014!!  #");
+	System.out.println("#                      #");
+	System.out.println("########################");
+	System.out.println("########################");
+	System.out.println("testing");
+	
+	//test/testtest
+	}
+
+}
