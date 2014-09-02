430aab4e9588aa590b1b7a36a408cf2f1912113a
bal217 <bal217@lehigh.edu>
2014-09-02 01:44:07 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Brittany Le Jouan
+//WelcomeClass Java Program
+//
+//define a class
+public class WelcomeClass{
+    public static void main(String[] args){
+        //print statement
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-B--A--L--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I went to track practice today and ran a lot.");
+    }
+    }
